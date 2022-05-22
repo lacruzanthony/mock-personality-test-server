7e773f289e62b1235763727333a1a2b75a7cb885
Anthony Lacruz <anthony.lacruz@riseinteractive.com>
2022-05-22 14:33:04 -0500

    feature/CRUD: card route

---
+++ src/routes/card.ts
@@ -0,0 +1,38 @@
+import { Router } from "express";
+import { CardModel, ICard } from "../models/card";
+
+const routes = Router();
+
+routes.get("/", async (req, res) => {
+  try {
+    const cards: ICard[] = await CardModel.find().exec();
+    return res.json(cards);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Sorry, something went wrong :/" });
+  }
+});
+
+routes.post("/", async (req, res) => {
+  try {
+    const card: ICard = req.body;
+
+    const cardExists = await CardModel.findOne({
+      name: card.headline
+    }).exec();
+
+    if (cardExists) {
+      return res
+        .status(409)
+        .json({ error: "There is already another card with this name" });
+    }
+
+    const newCard = await CardModel.create(card);
+    return res.status(201).json(newCard);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Sorry, something went wrong :/" });
+  }
+});
+
+export default routes;
